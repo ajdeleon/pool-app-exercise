c06cbc7ce28b23cfc4f0ff1a997775af023fb2ec
AJ de Leon <ajdeleon1064@gmail.com>
2019-07-03 10:46:06 -0400

    use http in api instance

--- client/src/api.js
+++ client/src/api.js
@@ -4,7 +4,7 @@ let apiURL
 if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
   apiURL = 'http://localhost:4000'
 } else {
-  apiURL = 'https://ajdeleon.xyz:4000'
+  apiURL = 'http://ajdeleon.xyz:4000'
 }
 
 const api = axios.create({
